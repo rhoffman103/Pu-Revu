5c0a16e1b2ce5f6a6f73150fc875c76ad660dd32
Andrew Harding <dreweharding@gmail.com>
2018-07-30 18:48:44 -0400

    created personal branch, attempted ajax request

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var location = "portsmouth";
+    var queryURL = "https://api.yelp.com/v3/businesses/search?location=" + location;
 
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: { "Authorization": "Bearer dgf0EwXxeGkIHfZLZrskaJo3I1elGrAPtof7Y0jXRxSPl3QTjr-hwHnuwWCCGVoWSwCOKxAqrW7V4KTdRjOk5Ng_ccri-5nyH04plUq5di4fpVVZcGnYaDN0D61cW3Yx" },
+    })
+        .then(function (response) {
+            console.log(response.data);
+
+        });
 
 });//End of document.ready function
