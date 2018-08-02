05276d5d1b1ec9c7f0bd4e387aa1a1efac1e6f76
Andrew Harding <dreweharding@gmail.com>
2018-08-01 20:24:15 -0400

    top three articles finished

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,41 +1,51 @@
 $(document).ready(function () {
+    // Initialize Materialize JS
+    M.AutoInit();
 
-    // $('select').formSelect();
+    // Top Three Most Popular Daily Articles
+    $(document).on("change", "#topic-select", function () {
 
-    var topicsArr = ["Arts", "Books", "Food", "Movies", "Sports"];
-    // Built by LucyBot. www.lucybot.com
-    var url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/" +
-        topicsArr[0] +
-        "/1.json";
-    url += '?' + $.param({
-        'api-key': "3aed9443566e4279ab5a39c67436378f"
+        $("#top-three").empty();
+        var topic = $("#topic-select").val();
+        // Built by LucyBot. www.lucybot.com
+        var url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/" +
+            topic +
+            "/1.json";
+        url += '?' + $.param({
+            'api-key': "3aed9443566e4279ab5a39c67436378f"
+        });
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+            var results = result.results;
+            for (var i = 0; i < 3; i++) {
+                var title = $("<h6>");
+                var caption = $("<p>");
+                var link = $("<a>");
+                link.attr("href", results[i].url);
+                link.attr("target", "_blank");
+                title.append(results[i].title);
+                caption.append(results[i].media[0].caption);
+                link.append(title);
+                $("#top-three").append(link);
+                $("#top-three").append(caption);
+            }
+        }).fail(function (err) {
+            throw err;
+        });
     });
+    // ^^^Top Three Most Popular Daily Articles^^^
 
+    var queryURL="https://icanhazdadjoke.com/";
     $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function (result) {
-        var results = result.results;
-        console.log(results[0]);
-        console.log(results[1]);
-        console.log(results[2]);
-        for (var i = 0; i < 3; i++) {
-            var title = $("<h6>");
-            var caption = $("<p>");
-            var link = $("<a>");
-            link.attr("href", results[i].url);
-            link.attr("target", "_blank");
-            console.log(results[i].url);
-            title.append(results[i].title);
-            console.log(results[i].title);
-            caption.append(results[i].media[0].caption);
-            console.log(results[i].media[0].caption);
-            link.append(title);
-            $("#top-three").append(link);
-            $("#top-three").append(caption);
-        }
-    }).fail(function (err) {
-        throw err;
-    });
+        url: queryURL,
+        method: "GET",
+        headers:{ 'Accept': 'text/plain'},
+      }).then(function(response) {
+          console.log(response);
+      })
+
 
 });//End of document.ready function
