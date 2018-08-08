7a7630aef59818029e8dc62a11e8b2688b1bb57b
Bobby H <rhoffman103@gmail.com>
2018-08-07 23:04:26 -0400

    added kyles code

--- assets/javascript/write-reviews.js
+++ assets/javascript/write-reviews.js
@@ -73,69 +73,90 @@ $(document).ready(function(){
         if (ratings.recommend === 1) {
             recommended = true;
         }
+        if ((zip.length == 0) || (name.length == 0)) {
+            $("#failed-submit").append(`<div class="col s12 red lighten-3">
+                                            <p>You must enter a Business and Zip to submit a review</p>
+                                        </div>`);
+        }
+        else {
 
-        //  MAKE A TIMESTAMP WITH MOMENT JS
-        var dateAdded = moment().format('lll'); 
+            //  MAKE A TIMESTAMP WITH MOMENT JS
+            var dateAdded = moment().format('lll'); 
 
-        //  COMPARE ALL BUSINESSES TO BUSINESS INPUT
-        var businessRef = database.ref("business");
-        businessRef.once('value', function(snapshot) {
-            // console.log(snapshot.val())
-            var businessLocationExists = false;
-            snapshot.forEach(function(childSnapshot) {
-                var childKey = childSnapshot.key;
-                var childData = childSnapshot.val();
-                // console.log(childKey);
-                // console.log(childData.name);
-                if ((name === childData.name) && (zip === childData.zip)) {
-                    // var str = comments.toString();
-                    // console.log(str);
-                    businessLocationExists = true;
-                    // console.log("Business Match!")
-                    // console.log(childData.name + " zip: " + childData.zip)
+            //  COMPARE ALL BUSINESSES TO BUSINESS INPUT
+            var businessRef = database.ref("business");
+            businessRef.once('value', function(snapshot) {
+                // console.log(snapshot.val())
+                var businessLocationExists = false;
+                snapshot.forEach(function(childSnapshot) {
+                    var childKey = childSnapshot.key;
+                    var childData = childSnapshot.val();
                     // console.log(childKey);
-                    // console.log(childData);
-                    // console.log(childData.comments);
-                    database.ref(`/business/${childKey}/ratings`).update({
-                        recommend: ratings.recommend + childData.ratings.recommend,
-                        oppose:    ratings.oppose + childData.ratings.oppose,
-                        clean:     ratings.clean + childData.ratings.clean,
-                        dirty:     ratings.dirty + childData.ratings.dirty,
-                        ok:        ratings.ok + childData.ratings.ok,
-                    });
-                    if (comment.length > 0) {
-                        // var dataComments = childData.comments;
-                        database.ref(`/business/${childKey}/comments`).push({
-                            comment, dateAdded, recommended
+                    // console.log(childData.name);
+                    if ((name === childData.name) && (zip === childData.zip)) {
+                        // var str = comments.toString();
+                        // console.log(str);
+                        businessLocationExists = true;
+                        // console.log("Business Match!")
+                        // console.log(childData.name + " zip: " + childData.zip)
+                        // console.log(childKey);
+                        // console.log(childData);
+                        // console.log(childData.comments);
+                        database.ref(`/business/${childKey}/ratings`).update({
+                            recommend: ratings.recommend + childData.ratings.recommend,
+                            oppose:    ratings.oppose + childData.ratings.oppose,
+                            clean:     ratings.clean + childData.ratings.clean,
+                            dirty:     ratings.dirty + childData.ratings.dirty,
+                            ok:        ratings.ok + childData.ratings.ok,
                         });
-                        console.log(comment)
+                        if (comment.length > 0) {
+                            // var dataComments = childData.comments;
+                            database.ref(`/business/${childKey}/comments`).push({
+                                comment, dateAdded, recommended
+                            });
+                            console.log(comment)
+                        }
+                        // if (comment.length > 0) {
+                        //     var dataComments = childData.comments;
+                        //     console.log(comments)
+                        //     var comment = comments.toString()
+                        //     console.log(comment)
+                        //     console.log(dataComments)
+                        //     database.ref(`/business/${childKey}`).update({
+                        //         comments,
+                        //         // comments: childData.comments.push(comments.toString())
+                        //     });
+                        // }
                     }
-                    // if (comment.length > 0) {
-                    //     var dataComments = childData.comments;
-                    //     console.log(comments)
-                    //     var comment = comments.toString()
-                    //     console.log(comment)
-                    //     console.log(dataComments)
-                    //     database.ref(`/business/${childKey}`).update({
-                    //         comments,
-                    //         // comments: childData.comments.push(comments.toString())
-                    //     });
-                    // }
+                });
+                if (businessLocationExists === false) {
+                    // Generate a reference to a new location and add some data using push()
+                    var newPushRef = database.ref("/business").push({
+                        zip, name, comments, ratings
+                    });
+                    // Get the unique key generated by push()
+                    var pushId = newPushRef.key;
+                    database.ref(`/business/${pushId}/comments`).push({
+                        comment, dateAdded, recommended
+                    });
+                    console.log("pushed new location");
                 }
             });
-            if (businessLocationExists === false) {
-                // Generate a reference to a new location and add some data using push()
-                var newPushRef = database.ref("/business").push({
-                    zip, name, comments, ratings
-                });
-                // Get the unique key generated by push()
-                var pushId = newPushRef.key;
-                database.ref(`/business/${pushId}/comments`).push({
-                    comment, dateAdded, recommended
-                });
-                console.log("pushed new location");
-            }
-        });
+
+            // CLEARS DIV ON SUBMIT EVENT && ADDS BUTTON TO WRITE ANOTHER
+
+            var anotherReview = "<a href='write-review.html' class='waves-effect waves-light blue darken-2 btn'>Write another review!</a>";
+
+            $("#page-content").empty();
+            $(".page-footer").empty();
+            $("#page-content").html(anotherReview);
+
+            // $("#z-input").val("");
+            // $("#business").val("");
+            // $(".comments").val("");
+            // $("li").removeClass("purple-text text-darken-2");
+
+        }
 
     });
     
